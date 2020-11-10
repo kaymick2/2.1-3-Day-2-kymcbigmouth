d7f1b812efa3311c81ba2e508c1558ae32868423
Kyle McDowell <kymcd21@icstudents.org>
2020-11-10 04:32:00 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+class Main {
+  public static void main(String[] args) {
+    //Kyle McDowell
+    //Variable exercises 2.1-2.3 day 2
+    int A = 1;
+    double B = 2.2;
+    System.out.println("A = " + A);
+    System.out.println("B = " + B);
+    int C = A;
+    double D = B;
+    System.out.println("");
+    System.out.println("C = " + C);
+    System.out.println("D = " + D);
+    System.out.println("");
+    double E = A + B;
+    double F = A - B;
+    double G = A * B;
+    double H = A / B;
+    System.out.println("E = A+B = " + E);
+    System.out.println("F = A-B = " + F);
+    System.out.println("G = A*B = " + G);
+    System.out.println("H = A/B = " + H);
+  }
+}
